53626ebf8850c227346c256b9925d6bf1b5bb2bd
kowsikashreeganesan <kowsikashreeganesan@gmail.com>
2026-09-12 16:07:57 +0530

    bio..py

---
+++ bio..py
@@ -0,0 +1,16 @@
+name="kowsikashree"
+age=18
+height=159.9
+learning_cybersecurity=True
+name=input("enter your name:")
+age=int(input("Enter your age:"))
+height=float(input("Enter your height:"))
+print(name)
+print(age)
+print(height)
+print(learning_cybersecurity)
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(learning_cybersecurity))
+learning_cybersecurity=input("is the statement true or not:")
