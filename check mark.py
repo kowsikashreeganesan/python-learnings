5977e2fe2d91ee4a2ea9337d6e7abe80507ea68e
kowsikashreeganesan <kowsikashreeganesan@gmail.com>
2026-09-25 21:58:25 +0530

    mark checking.py

---
+++ check mark.py
@@ -0,0 +1,14 @@
+mark = '85'
+if (mark >= '90'):
+    print('Grade A+');
+elif (mark >= '80'):
+    print('Grade A');
+elif (mark >= '70'):
+    print('Grade B');
+elif (mark >= '60'):
+    print('Grade C');
+elif (mark >= '50'):
+    print('Grade D');
+else:
+    print('Fail');
+
