fef9dbf2aaf14a60dde3df98f82e89a2d6d86db7
kowsikashreeganesan <kowsikashreeganesan@gmail.com>
2026-09-26 14:03:02 +0530

    for loop

---
+++ for loop.py
@@ -0,0 +1,7 @@
+for number in range(1,6):
+    print(number)
+tools = ["Nmap", "Wireshark", "BurpSuite"]
+for tools in tools:
+    print(tools)
+for number in range(1,11):
+    print("Checking item:", number)
